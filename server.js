29fd202f2fd0d7e7d394b46c9ed847943bb52040
itsFawad <fawadjack123@gmail.com>
2026-05-24 22:40:03 +0500

    Delete is working

--- server.js
+++ server.js
@@ -7,19 +7,25 @@ const mongoose = require("mongoose");
 const { connectDB } = require("./MVC/model/db");
 const { User } = require("./MVC/model/user");
 const { Recipe } = require("./MVC/model/recipes");
-const { signupErr, loginErr,createToken,auth,alreadyAuth,recipeError } = require("./MVC/middleware")
-const bcrypt = require('bcrypt');
+const {
+  signupErr,
+  loginErr,
+  createToken,
+  auth,
+  alreadyAuth,
+  recipeError,
+} = require("./MVC/middleware");
+const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser"); //without cookie-parser req.cookies give undefine
-
 
 //middleware
 app.use(express.json()); //use to make sure json is handle
-//use this middleware to get data directly from html form 
+//use this middleware to get data directly from html form
 app.use(express.urlencoded({ extended: true }));
 //use this middleware to allow relavent path
 app.use(express.static(__dirname + "/public"));
 //to make sure we dont get undefine jwt token error and instead redirect to login if not login
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 //we can put this code in dbconnect becasue we only want the server to listen when user is connected to db
 app.listen(PORT, (err) => {
@@ -35,7 +41,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); // this is to define that our ejs files are in this path from root
 
 //Routes
-app.get("/",auth, async (req, res) => {
+app.get("/", auth, async (req, res) => {
   try {
     // const data = await Recipe.find() //to get all the data from Recipe model (we apply function on model)
     const data = await Recipe.find().sort({ createdAt: -1 }); //sort in decending order
@@ -48,14 +54,14 @@ app.get("/",auth, async (req, res) => {
 });
 
 //login
-app.get("/login",alreadyAuth, (req, res) => {
-    const error = req.query.err
-    const msg = req.query.msg
-  res.render("login",{error: error , msg: msg});
+app.get("/login", alreadyAuth, (req, res) => {
+  const error = req.query.err;
+  const msg = req.query.msg;
+  res.render("login", { error: error, msg: msg });
 });
 
 //signup
-app.get("/signup",alreadyAuth, (req, res) => {
+app.get("/signup", alreadyAuth, (req, res) => {
   res.render("signup");
 });
 
@@ -79,70 +85,67 @@ app.get("/signup",alreadyAuth, (req, res) => {
 // });
 
 //post signup
-app.post("/signup",signupErr, async (req, res) => {
+app.post("/signup", signupErr, async (req, res) => {
   //get data from body
   const { username, email, pass } = req.body;
-//   console.log(data);
+  //   console.log(data);
 
   try {
     const saltRounds = 10;
-    const hashPass = await bcrypt.hash(pass,saltRounds)
+    const hashPass = await bcrypt.hash(pass, saltRounds);
     //make instance of User object
     const user = new User({
       username: username,
       email: email,
       password: hashPass,
     });
-   
+
     //now save it
     const result = await user.save();
-    
-    res.redirect('/login?msg=Register Successfully!');
+
+    res.redirect("/login?msg=Register Successfully!");
   } catch (error) {
     console.log(error);
-    res.redirect('/signup?msg=Something went wrong');
+    res.redirect("/signup?msg=Something went wrong");
   }
 });
 
 //login post
-app.post("/login",loginErr, async (req, res) => {
-
+app.post("/login", loginErr, async (req, res) => {
   const { email } = req.body;
-  const user = await User.findOne({ email: email});
+  const user = await User.findOne({ email: email });
   const token = createToken(user._id);
   const maxAge = 2 * 60;
   res.cookie("jwt", token, {
-     httpOnly: true,
-     maxAge: maxAge * 1000
+    httpOnly: true,
+    maxAge: maxAge * 1000,
   });
 
   res.redirect("/");
 });
 
 //recipe
-app.get("/recipe",auth, async (req, res) => {
-  res.render('recipes')
+app.get("/recipe", auth, async (req, res) => {
+  res.render("recipes");
 });
 
 //add recipe
-app.post('/recipe', auth, recipeError, async (req, res) => {
-
+app.post("/recipe", auth, recipeError, async (req, res) => {
   const { title, steps } = req.body;
 
   const recipe = new Recipe({
     author: req.user.id,
     title,
-    detail: steps
+    detail: steps,
   });
 
   const result = await recipe.save();
 
-  res.redirect('/showallrecipes');
-
+  res.redirect("/showallrecipes");
 });
 
 //show all recipes
-app.get("/showallrecipes",auth, async (req, res) => {
+app.get("/showallrecipes", auth, async (req, res) => {
   try {
     // const data = await Recipe.find() //to get all the data from Recipe model (we apply function on model)
     const data = await Recipe.find().sort({ createdAt: -1 }); //sort in decending order
@@ -154,27 +157,35 @@ app.get("/showallrecipes",auth, async (req, res) => {
   }
 });
 
-
 //get user posts
 app.get("/myrecipes", auth, async (req, res) => {
-  console.log(req.user)
 
-  const data = await Recipe.find({
-      author: req.user.id
-  }).sort({ createdAt: -1 });
+  try {
 
-  res.render("userPost", {
-      recipes: data
-  });
+    const recipes = await Recipe.find({
+      author: req.user.id,
+    }).sort({ createdAt: -1 });
+
+    return res.render("userPost", {
+      recipes: recipes || [],
+      error: recipes.length === 0 ? "No Data Available!" : null
+    });
+
+  } catch (error) {
+
+    return res.render("userPost", {
+      recipes: [],
+      error: "Something went wrong"
+    });
+
+  }
 
 });
-
 //delete user Post
 app.get("/postDelete/:id", async (req, res) => {
   await Recipe.findByIdAndDelete(req.params.id);
   res.redirect("/");
 });
-
 
 //if no page is found use this
 app.use((req, res) => {
